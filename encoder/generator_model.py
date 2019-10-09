6a2c31fa3f7140a741a4b916dae404c581d00ed3
Samantha Biegel <samantha_biegel@hotmail.com>
2019-10-09 16:15:36 +0200

    fix generator

--- encoder/generator_model.py
+++ encoder/generator_model.py
@@ -33,7 +33,7 @@ class Generator:
         self.set_dlatents(self.initial_dlatents)
 
         print(self.graph.get_operations())
-        self.generator_output = self.graph.get_tensor_by_name('G_synthesis_1/_Run/concat:0')
+        self.generator_output = self.graph.get_tensor_by_name('G_synthesis_1/_Run/concat/concat')
         self.generated_image = tflib.convert_images_to_uint8(self.generator_output, nchw_to_nhwc=True, uint8_cast=False)
         self.generated_image_uint8 = tf.saturate_cast(self.generated_image, tf.uint8)
 
